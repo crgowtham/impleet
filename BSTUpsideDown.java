f582ad6d79cf569d724851748301db7c623697a2
crgowtham <cr.gowtham@gmail.com>
2021-02-03 10:43:43 -0800

    Create BSTUpsideDown.java

---
+++ BSTUpsideDown.java
@@ -0,0 +1,53 @@
+/*
+Given the root of a binary tree, turn the tree upside down and return the new root.
+
+You can turn a binary tree upside down with the following steps:
+
+The original left child becomes the new root.
+The original root becomes the new right child.
+The original right child becomes the new left child.
+
+
+The mentioned steps are done level by level, it is guaranteed that every node in the given tree has either 0 or 2 children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: [4,5,2,null,null,3,1]
+*/
+
+public TreeNode upsideDownBinaryTree(TreeNode root) {
+    if(root == null || root.left == null) {
+        return root;
+    }
+    
+    TreeNode newRoot = upsideDownBinaryTree(root.left);
+    root.left.left = root.right;   // node 2 left children
+    root.left.right = root;         // node 2 right children
+    root.left = null;
+    root.right = null;
+    return newRoot;
+}
+
+public TreeNode upsideDownBinaryTree(TreeNode root) {
+    TreeNode curr = root;
+    TreeNode next = null;
+    TreeNode temp = null;
+    TreeNode prev = null;
+    
+    while(curr != null) {
+        next = curr.left;
+        
+        // swapping nodes now, need temp to keep the previous right child
+        curr.left = temp;
+        temp = curr.right;
+        curr.right = prev;
+        
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}  
