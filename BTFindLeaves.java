964561349be0f53fd51221845aa87f3139f74df8
crgowtham <cr.gowtham@gmail.com>
2021-02-03 11:29:42 -0800

    Create BTFindLeaves.java

---
+++ BTFindLeaves.java
@@ -0,0 +1,32 @@
+/*
+Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+
+ 
+
+Example:
+
+Input: [1,2,3,4,5]
+  
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+
+Output: [[4,5,3],[2],[1]]
+*/
+
+public class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        height(root, res);
+        return res;
+    }
+    private int height(TreeNode node, List<List<Integer>> res){
+        if(null==node)  return -1;
+        int level = 1 + Math.max(height(node.left, res), height(node.right, res));
+        if(res.size()<level+1)  res.add(new ArrayList<>());
+        res.get(level).add(node.val);
+        return level;
+    }
+}
