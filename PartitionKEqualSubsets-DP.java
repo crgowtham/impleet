2fa8ce7cd0945eee0e0f8c66319a65a6347be670
crgowtham <cr.gowtham@gmail.com>
2021-02-10 22:47:11 -0800

    Create PartitionKEqualSubsets-DP.java

---
+++ PartitionKEqualSubsets-DP.java
@@ -0,0 +1,43 @@
+/*
+Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
+
+ 
+
+Example 1:
+
+Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+Output: True
+Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
+*/
+
+class Solution {
+    public boolean search(int[] groups, int row, int[] nums, int target) {
+        if (row < 0) return true;
+        int v = nums[row--];
+        for (int i = 0; i < groups.length; i++) {
+            if (groups[i] + v <= target) {
+                groups[i] += v;
+                if (search(groups, row, nums, target)) return true;
+                groups[i] -= v;
+            }
+            if (groups[i] == 0) break;
+        }
+        return false;
+    }
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k > 0) return false;
+        int target = sum / k;
+
+        Arrays.sort(nums);
+        int row = nums.length - 1;
+        if (nums[row] > target) return false;
+        while (row >= 0 && nums[row] == target) {
+            row--;
+            k--;
+        }
+        return search(new int[k], row, nums, target);
+    }
+}
+
