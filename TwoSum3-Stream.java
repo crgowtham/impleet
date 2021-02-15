4f811435899443f2fd2c361561e4c0570d5e459e
crgowtham <cr.gowtham@gmail.com>
2021-02-14 20:59:36 -0800

    Create TwoSum3-Stream.java

---
+++ TwoSum3-Stream.java
@@ -0,0 +1,45 @@
+/*
+Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
+
+Implement the TwoSum class:
+
+TwoSum() Initializes the TwoSum object, with an empty array initially.
+void add(int number) Adds number to the data structure.
+boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+ 
+
+Example 1:
+
+Input
+["TwoSum", "add", "add", "add", "find", "find"]
+[[], [1], [3], [5], [4], [7]]
+Output
+[null, null, null, null, true, false]
+
+Explanation
+TwoSum twoSum = new TwoSum();
+twoSum.add(1);   // [] --> [1]
+twoSum.add(3);   // [1] --> [1,3]
+twoSum.add(5);   // [1,3] --> [1,3,5]
+twoSum.find(4);  // 1 + 3 = 4, return true
+twoSum.find(7);  // No two integers sum up to 7, return false
+*/
+
+public class TwoSum3-Stream {
+    private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    public void add(int number) {
+        map.put(number, map.containsKey(number) ? map.get(number) + 1 : 1);
+    }
+
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int i = entry.getKey();
+            int j = value - i;
+            if ((i == j && entry.getValue() > 1) || (i != j && map.containsKey(j))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
