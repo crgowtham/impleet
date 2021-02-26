ad212e87d53e3d99aa8aea7e8eb7623f375a96ed
crgowtham <cr.gowtham@gmail.com>
2021-02-26 12:00:10 -0800

    Update MinimumWindowSubString.java

--- MinimumWindowSubString.java
+++ MinimumWindowSubString.java
@@ -30,6 +30,32 @@ class Ideone
 		System.out.println("minWindow : " + minWindow("ADOBECODEBANC", "ABC"));
 	}
 	
+public String minWindow(String s, String t) {
+    int [] map = new int[128];
+    for (char c : t.toCharArray()) {
+      map[c]++;
+    }
+    int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
+    while (end < s.length()) {
+      final char c1 = s.charAt(end);
+      if (map[c1] > 0) counter--;
+      map[c1]--;
+      end++;
+      while (counter == 0) {
+        if (minLen > end - start) {
+          minLen = end - start;
+          minStart = start;
+        }
+        final char c2 = s.charAt(start);
+        map[c2]++;
+        if (map[c2] > 0) counter++;
+        start++;
+      }
+    }
+
+    return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+  }
+	
 static public String minWindow(String s, String t) {
     if(s == null || s.length() < t.length() || s.length() == 0){
         return "";
