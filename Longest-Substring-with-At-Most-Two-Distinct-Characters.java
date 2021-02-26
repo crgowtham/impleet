e19cc0783d579b85fc909553f9381d9693153a57
crgowtham <cr.gowtham@gmail.com>
2021-02-26 12:03:19 -0800

    Update Longest-Substring-with-At-Most-Two-Distinct-Characters.java

--- Longest-Substring-with-At-Most-Two-Distinct-Characters.java
+++ Longest-Substring-with-At-Most-Two-Distinct-Characters.java
@@ -24,3 +24,30 @@
 
     return maxLen;
   }
+
+/*
+LongestSubstring - without repeating characters
+*/
+
+ public int lengthOfLongestSubstring2(String s) {
+    int[] map = new int[128];
+    int start = 0, end = 0, maxLen = 0, counter = 0;
+
+    while (end < s.length()) {
+      final char c1 = s.charAt(end);
+      if (map[c1] > 0) counter++;
+      map[c1]++;
+      end++;
+
+      while (counter > 0) {
+        final char c2 = s.charAt(start);
+        if (map[c2] > 1) counter--;
+        map[c2]--;
+        start++;
+      }
+
+      maxLen = Math.max(maxLen, end - start);
+    }
+
+    return maxLen;
+  }
