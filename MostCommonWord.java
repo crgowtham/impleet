9f27c9d3cfd1d5d953ef04e07f23323f081752eb
crgowtham <cr.gowtham@gmail.com>
2021-03-01 15:46:08 -0800

    Create MostCommonWord.java

---
+++ MostCommonWord.java
@@ -0,0 +1,46 @@
+/*
+Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+
+Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
+
+ 
+
+Example:
+
+Input: 
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+Output: "ball"
+Explanation: 
+"hit" occurs 3 times, but it is a banned word.
+"ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+Note that words in the paragraph are not case sensitive,
+that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+and that "hit" isn't the answer even though it occurs more because it is banned.
+*/
+
+class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+
+        // 1). replace the punctuations with spaces,
+        // and put all letters in lower case
+        String normalizedStr = paragraph.replaceAll("[^a-zA-Z0-9 ]", " ").toLowerCase();
+
+        // 2). split the string into words
+        String[] words = normalizedStr.split("\\s+");
+
+        Set<String> bannedWords = new HashSet();
+        for (String word : banned)
+            bannedWords.add(word);
+
+        Map<String, Integer> wordCount = new HashMap();
+        // 3). count the appearance of each word, excluding the banned words
+        for (String word : words) {
+            if (!bannedWords.contains(word))
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        // 4). return the word with the highest frequency
+        return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
