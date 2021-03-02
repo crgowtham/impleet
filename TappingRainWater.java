63c6eb6c3f03c7412449294361320478b6574c70
crgowtham <cr.gowtham@gmail.com>
2021-03-01 16:04:47 -0800

    Create TappingRainWater.java

---
+++ TappingRainWater.java
@@ -0,0 +1,39 @@
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+*/
+
+class TappingRainWater {
+    public int trap(int[] height) {
+        // time : O(n)
+        // space : O(1)
+        if (height.length==0) return 0; 
+        int left = 0, right = height.length-1; 
+        int leftMax=0, rightMax=0; 
+        int ans = 0; 
+        while (left < right) {
+            if (height[left] > leftMax) leftMax = height[left]; 
+            if (height[right] > rightMax) rightMax = height[right];
+            if (leftMax < rightMax) {
+                ans += Math.max(0, leftMax-height[left]); 
+                left++; 
+            } else {
+                ans += Math.max(0, rightMax-height[right]); 
+                right--; 
+            }
+        }
+        return ans; 
+    }
+}
