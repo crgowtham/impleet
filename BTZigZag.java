8ac039fc98218386f0e1c41d3a629fb22e1f78ae
crgowtham <cr.gowtham@gmail.com>
2021-03-09 15:05:50 -0800

    Create BTZigZag.java

---
+++ BTZigZag.java
@@ -0,0 +1,47 @@
+/*
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  protected void DFS(TreeNode node, int level, List<List<Integer>> results) {
+    if (level >= results.size()) {
+      LinkedList<Integer> newLevel = new LinkedList<Integer>();
+      newLevel.add(node.val);
+      results.add(newLevel);
+    } else {
+      if (level % 2 == 0)
+        results.get(level).add(node.val);
+      else
+        results.get(level).add(0, node.val);
+    }
+
+    if (node.left != null) DFS(node.left, level + 1, results);
+    if (node.right != null) DFS(node.right, level + 1, results);
+  }
+
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<List<Integer>>();
+    }
+    List<List<Integer>> results = new ArrayList<List<Integer>>();
+    DFS(root, 0, results);
+    return results;
+  }
+}
