557d77c8db8bdb33e8254cd095ffe5c2b089dc0b
crgowtham <cr.gowtham@gmail.com>
2021-03-08 23:56:03 -0800

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,24 @@
+/*
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+*/
+
+public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null) {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
