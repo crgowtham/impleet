2cdd2d5812a7158510a9092369d2b9dea39c989c
crgowtham <cr.gowtham@gmail.com>
2021-03-14 23:54:47 -0700

    Create BTSerializeDeSerialize.java

---
+++ BTSerializeDeSerialize.java
@@ -0,0 +1,88 @@
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+Input: root = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+*/
+
+/* Definition for a binary tree node. */
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
+
+// Serialization
+public class Codec {
+  public String rserialize(TreeNode root, String str) {
+    // Recursive serialization.
+    if (root == null) {
+      str += "null,";
+    } else {
+      str += str.valueOf(root.val) + ",";
+      str = rserialize(root.left, str);
+      str = rserialize(root.right, str);
+    }
+    return str;
+  }
+
+  // Encodes a tree to a single string.
+  public String serialize(TreeNode root) {
+    return rserialize(root, "");
+  }
+};
+
+public class Codec {
+  public TreeNode rdeserialize(List<String> l) {
+    // Recursive deserialization.
+    if (l.get(0).equals("null")) {
+      l.remove(0);
+      return null;
+    }
+
+    TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+    l.remove(0);
+    root.left = rdeserialize(l);
+    root.right = rdeserialize(l);
+
+    return root;
+  }
+
+  // Decodes your encoded data to tree.
+  public TreeNode deserialize(String data) {
+    String[] data_array = data.split(",");
+    List<String> data_list = new LinkedList<String>(Arrays.asList(data_array));
+    return rdeserialize(data_list);
+  }
+};
