cf659447cc02cfb2092794d31fab559f2aec8fc1
crgowtham <cr.gowtham@gmail.com>
2021-03-27 13:46:53 -0700

    Update Hashmap.java

--- Hashmap.java
+++ Hashmap.java
@@ -20,53 +20,90 @@ hashMap.remove(2);          // remove the mapping for 2
 hashMap.get(2);            // returns -1 (not found) 
 */
 
- class MyHashMap {
-        final ListNode[] nodes = new ListNode[10000];
+class Pair<U, V> {
+  public U first;
+  public V second;
 
-        public void put(int key, int value) {
-            int i = idx(key);
-            if (nodes[i] == null)
-                nodes[i] = new ListNode(-1, -1);
-            ListNode prev = find(nodes[i], key);
-            if (prev.next == null)
-                prev.next = new ListNode(key, value);
-            else prev.next.val = value;
-        }
+  public Pair(U first, V second) {
+    this.first = first;
+    this.second = second;
+  }
+}
 
-        public int get(int key) {
-            int i = idx(key);
-            if (nodes[i] == null)
-                return -1;
-            ListNode node = find(nodes[i], key);
-            return node.next == null ? -1 : node.next.val;
-        }
 
-        public void remove(int key) {
-            int i = idx(key);
-            if (nodes[i] == null) return;
-            ListNode prev = find(nodes[i], key);
-            if (prev.next == null) return;
-            prev.next = prev.next.next;
-        }
+class Bucket {
+  private List<Pair<Integer, Integer>> bucket;
 
-        int idx(int key) { return Integer.hashCode(key) % nodes.length;}
+  public Bucket() {
+    this.bucket = new LinkedList<Pair<Integer, Integer>>();
+  }
 
-        ListNode find(ListNode bucket, int key) {
-            ListNode node = bucket, prev = null;
-            while (node != null && node.key != key) {
-                prev = node;
-                node = node.next;
-            }
-            return prev;
-        }
-
-        class ListNode {
-            int key, val;
-            ListNode next;
-
-            ListNode(int key, int val) {
-                this.key = key;
-                this.val = val;
-            }
-        }
+  public Integer get(Integer key) {
+    for (Pair<Integer, Integer> pair : this.bucket) {
+      if (pair.first.equals(key))
+        return pair.second;
     }
+    return -1;
+  }
+
+  public void update(Integer key, Integer value) {
+    boolean found = false;
+    for (Pair<Integer, Integer> pair : this.bucket) {
+      if (pair.first.equals(key)) {
+        pair.second = value;
+        found = true;
+      }
+    }
+    if (!found)
+      this.bucket.add(new Pair<Integer, Integer>(key, value));
+  }
+
+  public void remove(Integer key) {
+    for (Pair<Integer, Integer> pair : this.bucket) {
+      if (pair.first.equals(key)) {
+        this.bucket.remove(pair);
+        break;
+      }
+    }
+  }
+}
+
+class MyHashMap {
+  private int key_space;
+  private List<Bucket> hash_table;
+
+  /** Initialize your data structure here. */
+  public MyHashMap() {
+    this.key_space = 2069;
+    this.hash_table = new ArrayList<Bucket>();
+    for (int i = 0; i < this.key_space; ++i) {
+      this.hash_table.add(new Bucket());
+    }
+  }
+
+  /** value will always be non-negative. */
+  public void put(int key, int value) {
+    int hash_key = key % this.key_space;
+    this.hash_table.get(hash_key).update(key, value);
+  }
+
+  /**
+   * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
+   * for the key
+   */
+  public int get(int key) {
+    int hash_key = key % this.key_space;
+    return this.hash_table.get(hash_key).get(key);
+  }
+
+  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+  public void remove(int key) {
+    int hash_key = key % this.key_space;
+    this.hash_table.get(hash_key).remove(key);
+  }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such: MyHashMap obj = new MyHashMap();
+ * obj.put(key,value); int param_2 = obj.get(key); obj.remove(key);
+ */
