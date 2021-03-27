108884f0922cbc289e98d39d0ab37e8debf37943
crgowtham <cr.gowtham@gmail.com>
2021-03-27 14:16:15 -0700

    Update IsomorphicStrings.java

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -28,6 +28,23 @@ class Ideone
 		System.out.println("isIsomorphic: " + isIsomorphic("abb", "caa"));
 	}
 	
+	class Solution {
+	public:
+	    bool isIsomorphic(string s, string t) {
+		int len = s.length();
+		int m1[256], m2[256];
+		for (int i = 0; i < 256; i++) {
+		    m1[i] = m2[i] = -1;
+		}
+
+		for (int i = 0; i < len; i++) {
+		    if (m1[s[i]] != m2[t[i]]) return false;
+		    m1[s[i]] = m2[t[i]] = i;
+		}
+		return true;
+	    }
+	};
+	
 	static public boolean isIsomorphic(String s1, String s2) {
         int[] m = new int[512];
         for (int i = 0; i < s1.length(); i++) {
