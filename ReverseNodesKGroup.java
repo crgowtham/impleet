70c535bf02f26790112de9f11b66221010c992ca
crgowtham <cr.gowtham@gmail.com>
2021-03-27 13:18:45 -0700

    Update ReverseNodesKGroup.java

--- ReverseNodesKGroup.java
+++ ReverseNodesKGroup.java
@@ -26,68 +26,24 @@ Output: [2,1,4,3,5]
  *     ListNode(int x) { val = x; }
  * }
  */
-class Solution {
-    
-    public ListNode reverseLinkedList(ListNode head, int k) {
-        
-        // Reverse k nodes of the given linked list.
-        // This function assumes that the list contains 
-        // atleast k nodes.
-        ListNode new_head = null;
-        ListNode ptr = head;
-        
-        while (k > 0) {
-            
-            // Keep track of the next node to process in the
-            // original list
-            ListNode next_node = ptr.next;
-            
-            // Insert the node pointed to by "ptr"
-            // at the beginning of the reversed list
-            ptr.next = new_head;
-            new_head = ptr;
-            
-            // Move on to the next node
-            ptr = next_node;
-            
-            // Decrement the count of nodes to be reversed by 1
-            k--;
-        }
-            
-            
-        // Return the head of the reversed list
-        return new_head;
-    
+public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode curr = head;
+    int count = 0;
+    while (curr != null && count != k) { // find the k+1 node
+        curr = curr.next;
+        count++;
     }
-            
-    public ListNode reverseKGroup(ListNode head, int k) {
-        
-        int count = 0;
-        ListNode ptr = head;
-        
-        // First, see if there are atleast k nodes
-        // left in the linked list.
-        while (count < k && ptr != null) {
-            ptr = ptr.next;
-            count++;
+    if (count == k) { // if k+1 node is found
+        curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
+        // head - head-pointer to direct part, 
+        // curr - head-pointer to reversed part;
+        while (count-- > 0) { // reverse current k-group: 
+            ListNode tmp = head.next; // tmp - next head in direct part
+            head.next = curr; // preappending "direct" head to the reversed list 
+            curr = head; // move head of reversed part to a new node
+            head = tmp; // move "direct" head to the next node in direct part
         }
-            
-        
-        // If we have k nodes, then we reverse them
-        if (count == k) {
-            
-            // Reverse the first k nodes of the list and
-            // get the reversed list's head.
-            ListNode reversedHead = this.reverseLinkedList(head, k);
-            
-            // Now recurse on the remaining linked list. Since
-            // our recursion returns the head of the overall processed
-            // list, we use that and the "original" head of the "k" nodes
-            // to re-wire the connections.
-            head.next = this.reverseKGroup(ptr, k);
-            return reversedHead;
-        }
-            
-        return head;
+        head = curr;
     }
+    return head;
 }
