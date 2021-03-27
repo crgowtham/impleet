1e5f03a1e594cef75e10b1c776759d8a44d7a79b
crgowtham <cr.gowtham@gmail.com>
2021-03-27 12:59:56 -0700

    Update SearchInRotatedArray.java

--- SearchInRotatedArray.java
+++ SearchInRotatedArray.java
@@ -13,26 +13,25 @@ Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
 */
 
-public class Solution {
+class Solution {
   public int search(int[] nums, int target) {
-      int start = 0, end = nums.length - 1;
-      while (start < end) {
-          int mid = (start + end) / 2;
-          if (nums[mid] > nums[end]) {  // eg. 3,4,5,6,1,2
-              if (target > nums[mid] || target <= nums[end]) {
-                  start = mid + 1;
-              } else {
-                  end = mid;
-              }
-          } else {  // eg. 5,6,1,2,3,4
-              if (target > nums[mid] && target <= nums[end]) {
-                  start = mid + 1;
-              } else {
-                  end = mid;
-              }
-          }
+    int start = 0, end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target) return mid;
+      else if (nums[mid] >= nums[start]) {
+        if (target >= nums[start] && target < nums[mid]) 
+         end = mid - 1;
+        else 
+         start = mid + 1;
       }
-      if (start == end && target != nums[start]) return -1;
-      return start;
+      else {
+        if (target <= nums[end] && target > nums[mid]) 
+         start = mid + 1;
+        else 
+         end = mid - 1;
+      }
+    }
+    return -1;
   }
 }
