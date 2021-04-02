e29c5e8295e48d4e4a2ad529743fe82760f19d81
crgowtham <cr.gowtham@gmail.com>
2021-04-01 19:50:33 -0700

    Update MergeKSortedList.java

--- MergeKSortedList.java
+++ MergeKSortedList.java
@@ -22,41 +22,30 @@ Approach 5: Merge with Divide And Conquer
 */
 
 
-
-public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    ListNode h = new ListNode(0);
-    ListNode ans=h;
-    while (l1 != null && l2 != null) {
-        if (l1.val < l2.val) {
-            h.next = l1;
-            h = h.next;
-            l1 = l1.next;
-        } else {
-            h.next = l2;
-            h = h.next;
-            l2 = l2.next;
-        }
-    }
-    if(l1==null){
-        h.next=l2;
-    }
-    if(l2==null){
-        h.next=l1;
-    } 
-    return ans.next;
+public static ListNode mergeKLists(ListNode[] lists){
+    return partion(lists,0,lists.length-1);
 }
-public ListNode mergeKLists(ListNode[] lists) {
-    if(lists.length==0){
-        return null;
-    }
-    int interval = 1;
-    while(interval<lists.length){
-        System.out.println(lists.length);
-        for (int i = 0; i + interval< lists.length; i=i+interval*2) {
-            lists[i]=mergeTwoLists(lists[i],lists[i+interval]);            
-        }
-        interval*=2;
-    }
 
-    return lists[0];
+public static ListNode partion(ListNode[] lists,int s,int e){
+    if(s==e)  return lists[s];
+    if(s<e){
+        int q=(s+e)/2;
+        ListNode l1=partion(lists,s,q);
+        ListNode l2=partion(lists,q+1,e);
+        return merge(l1,l2);
+    }else
+        return null;
+}
+
+//This function is from Merge Two Sorted Lists.
+public static ListNode merge(ListNode l1,ListNode l2){
+    if(l1==null) return l2;
+    if(l2==null) return l1;
+    if(l1.val<l2.val){
+        l1.next=merge(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=merge(l1,l2.next);
+        return l2;
+    }
 }
