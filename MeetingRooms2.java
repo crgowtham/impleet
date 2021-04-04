e3f7ebe802fafe45609fb5424ce088cacfead4e5
crgowtham <cr.gowtham@gmail.com>
2021-04-03 23:32:28 -0700

    Update MeetingRooms2.java

--- MeetingRooms2.java
+++ MeetingRooms2.java
@@ -14,50 +14,24 @@ Output: 1
 
 */
 
-class MeetingRooms2 {
-    public int minMeetingRooms(int[][] intervals) {
-        
-    // Check for the base case. If there are no intervals, return 0
-    if (intervals.length == 0) {
-      return 0;
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0;
+        int endsItr = 0;
+        for(int i=0; i<starts.length; i++) {
+            if(starts[i]<ends[endsItr])
+                rooms++;
+            else
+                endsItr++;
+        }
+        return rooms;
     }
-
-    // Min heap
-    PriorityQueue<Integer> allocator =
-        new PriorityQueue<Integer>(
-            intervals.length,
-            new Comparator<Integer>() {
-              public int compare(Integer a, Integer b) {
-                return a - b;
-              }
-            });
-
-    // Sort the intervals by start time
-    Arrays.sort(
-        intervals,
-        new Comparator<int[]>() {
-          public int compare(final int[] a, final int[] b) {
-            return a[0] - b[0];
-          }
-        });
-
-    // Add the first meeting
-    allocator.add(intervals[0][1]);
-
-    // Iterate over remaining intervals
-    for (int i = 1; i < intervals.length; i++) {
-
-      // If the room due to free up the earliest is free, assign that room to this meeting.
-      if (intervals[i][0] >= allocator.peek()) {
-        allocator.poll();
-      }
-
-      // If a new room is to be assigned, then also we add to the heap,
-      // If an old room is allocated, then also we have to add to the heap with updated end time.
-      allocator.add(intervals[i][1]);
-    }
-
-    // The size of the heap tells us the minimum rooms required for all the meetings.
-    return allocator.size();
-  }
 }
