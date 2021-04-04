243fea8f6819f0f1fff19ff6180f78cd1acabe06
crgowtham <cr.gowtham@gmail.com>
2021-04-03 22:21:58 -0700

    Create AirlineTicket-Amazon.java

---
+++ AirlineTicket-Amazon.java
@@ -0,0 +1,20 @@
+/*
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], 
+reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+*/
+
+public List<String> findItinerary(String[][] tickets) {
+    for (String[] ticket : tickets)
+        targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+    visit("JFK");
+    return route;
+}
+
+Map<String, PriorityQueue<String>> targets = new HashMap<>();
+List<String> route = new LinkedList();
+
+void visit(String airport) {
+    while(targets.containsKey(airport) && !targets.get(airport).isEmpty())
+        visit(targets.get(airport).poll());
+    route.add(0, airport);
+}
