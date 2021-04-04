e9ec06c5a4e86150cecb496d5530958557dfb294
crgowtham <cr.gowtham@gmail.com>
2021-04-03 22:12:10 -0700

    Create EdgesCreateTree-Amazon.java

---
+++ EdgesCreateTree-Amazon.java
@@ -0,0 +1,32 @@
+/*
+Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of nodes), 
+write a function to check whether these edges make up a valid tree
+*/
+
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // initialize n isolated islands
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+        
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+            
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+            
+            // union
+            nums[x] = y;
+        }
+        
+        return edges.length == n - 1;
+    }
+    
+    int find(int nums[], int i) {
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
+    }
+}
